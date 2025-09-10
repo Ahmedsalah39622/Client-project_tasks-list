9952e25554c565b442738e1c6240e227b445bbea
Ahmedsalah39622 <66878342+Ahmedsalah39622@users.noreply.github.com>
2025-09-10 13:58:11 +0300

    Update assembly informational version and cache

--- Company_representative_tasks/obj/Debug/net9.0/Company_representative_tasks.AssemblyInfo.cs
+++ Company_representative_tasks/obj/Debug/net9.0/Company_representative_tasks.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Company_representative_tasks")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95d70c4c138d7b87caa124e8b48586da9d5e79d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93e7fee0647f408521415a5f17c75502ee335e4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Company_representative_tasks")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Company_representative_tasks")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
